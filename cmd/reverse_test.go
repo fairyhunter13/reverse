35776e56ac6273249957082ebb057df135b1936a
Lunny Xiao <xiaolunwen@gmail.com>
2020-03-10 07:54:38 +0000

    Support reverse from reader (#7)
    Support reverse from reader
    
    Reviewed-on: https://gitea.com/xorm/reverse/pulls/7

--- cmd/reverse_test.go
+++ cmd/reverse_test.go
@@ -7,10 +7,14 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
+	"xorm.io/xorm"
 )
 
 var result = fmt.Sprintf(`package models
@@ -31,4 +35,39 @@ func TestReverse(t *testing.T) {
 	bs, err := ioutil.ReadFile("../models/models.go")
 	assert.NoError(t, err)
 	assert.EqualValues(t, result, string(bs))
+}
+
+func TestReverse2(t *testing.T) {
+	type Outfw struct {
+		Id       int    `xorm:"not null pk autoincr"`
+		Sql      string `xorm:"default '' TEXT"`
+		Template string `xorm:"default '' TEXT"`
+		Filename string `xorm:"VARCHAR(50)"`
+	}
+
+	dir, err := ioutil.TempDir(os.TempDir(), "reverse")
+	assert.NoError(t, err)
+
+	e, err := xorm.NewEngine("sqlite3", filepath.Join(dir, "db.db"))
+	assert.NoError(t, err)
+
+	assert.NoError(t, e.Sync2(new(Outfw)))
+
+	err = reverseFromReader(strings.NewReader(`
+kind: reverse
+name: mydb
+source:
+  database: sqlite3
+  conn_str: '../testdata/test.db'
+targets:
+- type: codes
+  include_tables:
+  - a
+  - b
+  exclude_tables:
+  - c
+  language: golang
+  output_dir: ../models
+`))
+	assert.NoError(t, err)
 }
